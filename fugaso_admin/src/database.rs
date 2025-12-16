27f6c082d6b7e5b23ad570f13d6dc01605949972
Boria <bossman0072@gmail.com>
2025-12-16 15:50:54 +0300

    delete games from db

--- fugaso_admin/src/database.rs
+++ fugaso_admin/src/database.rs
@@ -2,25 +2,14 @@ use fugaso_data::fugaso_game;
 use sea_orm::{ConnectionTrait, EntityTrait, InsertResult, IntoActiveModel};
 
 pub async fn insert_games<C: ConnectionTrait>(db: &C) -> Result<InsertResult<fugaso_data::fugaso_game::ActiveModel>, sea_orm::DbErr> {
-    let games = vec![
-        fugaso_game::Model {
-            id: 44,
-            display_name: Some("Thunder Express".to_string()),
-            game_name: Some("thunderexpress".to_string()),
-            math_class: "ThunderExpressMath".to_string(),
-            origin: "LOCAL".to_string(),
-            promo: true,
-            ..Default::default()
-        },
-        fugaso_game::Model {
-            id: 45,
-            display_name: Some("Electric Coins 2".to_string()),
-            game_name: Some("electriccoins2".to_string()),
-            math_class: "ElectricCoins2Math".to_string(),
-            origin: "LOCAL".to_string(),
-            promo: true,
-            ..Default::default()
-        },
-    ];
+    let games = vec![fugaso_game::Model {
+        id: 44,
+        display_name: Some("Thunder Express".to_string()),
+        game_name: Some("thunderexpress".to_string()),
+        math_class: "ThunderExpressMath".to_string(),
+        origin: "LOCAL".to_string(),
+        promo: true,
+        ..Default::default()
+    }];
     fugaso_game::Entity::insert_many(games.into_iter().map(|m| m.into_active_model())).exec(db).await
 }
